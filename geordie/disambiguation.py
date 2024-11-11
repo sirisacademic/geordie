be836319b18e4ca046544c2680bd98883a376c8b
bertagrim <berta.grimau@sirisacademic.com>
2024-11-11 20:20:23 +0100

    Modified class 'disambiguation' to filter out results that do not surpass a certain threshold probability.

--- geordie/disambiguation.py
+++ geordie/disambiguation.py
@@ -16,30 +16,35 @@ class EntityLinker:
             entity = item['entity_normalised']
             try:
                 res = self.app.geocode(entity, addressdetails=True, language ="en", extratags=True)
-
-                # Extracting subset of keys with fallback for missing keys
-                subset = {key: res.raw.get(key, None) for key in keys_to_extract}
-
-                if res["raw"].get("address", None) is not None:
-                    address = res["raw"].get("address", None)
-                    subset["entity_type"] = list(address.keys())[0]
+                
+                # Filter out results that do not surpass a certain threshold probability to avoid false positives.
+                if res.raw["importance"]>=0.6: # Change importance threshold as needed.
+                    
                     # Extracting subset of keys with fallback for missing keys
-                    address_subset = {key: address.get(key, None) for key in address_keys_to_extract}
-                else:
-                    # Adding null results for non-existing keys, so all results have the same structure.
-                    address_subset = {key: None for key in address_keys_to_extract}
-                subset.update(address_subset)
+                    subset = {key: res.raw.get(key, None) for key in keys_to_extract}
 
-                if res["raw"].get("extratags", None) is not None:
-                    extratags = res["raw"].get("extratags", None)
-                    # Extracting subset of keys with fallback for missing keys
-                    extratags_subset = {key: extratags.get(key, None) for key in extratags_keys_to_extract}
-                else:
-                    # Adding null results for non-existing keys, so all results have the same structure.
-                    extratags_subset = {key: None for key in extratags_keys_to_extract}
-                subset.update(extratags_subset)
+                    if res.raw.get("address", None) is not None:
+                        address = res.raw.get("address", None)
+                        subset["entity_type"] = list(address.keys())[0]
+                        # Extracting subset of keys with fallback for missing keys
+                        address_subset = {key: address.get(key, None) for key in address_keys_to_extract}
+                    else:
+                        # Adding null results for non-existing keys, so all results have the same structure.
+                        address_subset = {key: None for key in address_keys_to_extract}
+                    subset.update(address_subset)
 
-                item['osm'] = subset
+                    if res.raw.get("extratags", None) is not None:
+                        extratags = res.raw.get("extratags", None)
+                        # Extracting subset of keys with fallback for missing keys
+                        extratags_subset = {key: extratags.get(key, None) for key in extratags_keys_to_extract}
+                    else:
+                        # Adding null results for non-existing keys, so all results have the same structure.
+                        extratags_subset = {key: None for key in extratags_keys_to_extract}
+                    subset.update(extratags_subset)
+
+                    item['osm'] = subset
+                else:
+                    item["osm"] = None
             except:
                 item['osm'] = None
             result.append(item)
